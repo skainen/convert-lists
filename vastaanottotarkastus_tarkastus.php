59767e01648faa0084fb0be8f37fdc991c76b8c4
skainen <109695772+skainen@users.noreply.github.com>
2023-07-08 11:20:14 +0300

    Update vastaanottotarkastus_tarkastus.php

--- vastaanottotarkastus_tarkastus.php
+++ vastaanottotarkastus_tarkastus.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://www.istekki.fi/istekki-theme-v2/images/favicon.ico" type="image/x-icon" rel="icon">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="lomake.css">
 <title>Vantaanottotarkastuksen tarkastus</title>
