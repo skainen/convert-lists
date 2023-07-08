590a82321725014a51db499765b0097afa177a8f
skainen <109695772+skainen@users.noreply.github.com>
2023-07-08 11:15:25 +0300

    Update etusivu.php

--- etusivu.php
+++ etusivu.php
@@ -3,7 +3,6 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
-<link href="https://www.istekki.fi/istekki-theme-v2/images/favicon.ico" type="image/x-icon" rel="icon">
 <link rel="stylesheet" href="lomake.css">
 <title>Etusivu</title>
 </head>
